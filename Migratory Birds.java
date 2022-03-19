a1c5ae980f7a9057c7a91c8432c069003396dd5f
PRIYA NIMONKAR <73404350+pni151@users.noreply.github.com>
2022-03-19 19:57:12 +0530

    Create Migratory Birds.java

---
+++ Migratory Birds.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int migratoryBirds(List<Integer> arr) {
+    // Write your code here
+    int ar[] = new int[5]; //create array to store the freq
+    for (int i=0; i<arr.size();i++){//iterate till end 
+       //particular index values gets incremented
+        ar [arr.get(i)-1]++;  //1 is minus becoz array starts from 0 to 4
+        }
+    int max = 0,ans=0; 
+    
+    for (int i=0;i<ar.length; i++){
+        if(ar[i]>max){ 
+            /*even if 2 or more has highest freq the lowest indx will be returned as we are not checking for = , its just greater than
+            */
+            max = ar[i]; 
+            ans = i+1; //earlier one was subtracted
+        }    
+    }
+    return ans;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.migratoryBirds(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
