600b4925b93c4b5eb62622c2d7fbee7408048f0f
PRIYA NIMONKAR <73404350+pni151@users.noreply.github.com>
2022-06-05 16:37:32 +0530

    Create Designer PDF Viewer.java

---
+++ Designer PDF Viewer.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<Integer> h, String word) {
+        int tallLetter = 0;
+        //iterate over the word
+        for (int i = 0; i< word.length(); i++){
+            //get the each letters height
+            int currheight = h.get((int) (word.charAt(i) - 'a'));
+            //compare it with the tallletter and reassign if needed
+            if(currheight > tallLetter){
+                tallLetter = currheight;
+            }   
+        }
+        //return the product of length and tallletter
+        return tallLetter*word.length();        
+    // Write your code here  
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] hTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> h = new ArrayList<>();
+
+        for (int i = 0; i < 26; i++) {
+            int hItem = Integer.parseInt(hTemp[i]);
+            h.add(hItem);
+        }
+
+        String word = bufferedReader.readLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
