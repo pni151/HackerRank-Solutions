5a0777d3fa41f44ee43bc2835bc4f631dfcb91a0
PRIYA NIMONKAR <73404350+pni151@users.noreply.github.com>
2022-06-05 19:03:13 +0530

    Create Utopian Tree.java

---
+++ Utopian Tree.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'utopianTree' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int utopianTree(int n) {
+    // Write your code here
+    int ans = 1;  //as height will be 1 at the start
+    if(n==0) return ans;
+    else {
+        //till n is greater than 0
+        while(n>0){
+            //when odd
+            if (ans % 2 == 1){
+                ans = ans* 2;
+            }
+            //when even
+            else{
+                ans += 1;
+            }
+            n--; //decrement the cycle count
+        }
+        return ans;       
+    }
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+            int result = Result.utopianTree(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
